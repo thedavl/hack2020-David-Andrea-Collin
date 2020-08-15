5d2dffec48897a10a07cdd32e70f7404ef47ed97
David Lee <thedavl2001@gmail.com>
2020-08-15 14:59:40 -0700

    setup basic workout endpoint, no database yet

--- backend/api/routes/workouts.js
+++ backend/api/routes/workouts.js
@@ -2,11 +2,28 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    
+    res.status(200).json({
+        message: 'GET request to workouts endpoint'
+    });
 });
 
 router.get('/:workoutId', (req, res, next) => {
     const id = req.params.workoutId;
+    res.status(200).json({
+        message: 'GET request to SPECIFIC WORKOUT endpoint',
+        id: id
+    });
 });
+
+router.post('/', (req, res, next) => {
+    const workout = {
+        title: req.body.title,
+        description: req.body.description
+    };
+    res.status(200).json({
+        message: 'POST to workout endpoint',
+        createdWorkout: workout
+    })
+})
 
 module.exports = router;
--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,27 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+const bodyparser = require('body-parser');
 
 const workoutRoutes = require('./api/routes/workouts');
+
+app.use(morgan('dev'));
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+
+app.use((req, res, next) => {
+    res.header("access-control-allow-origin", "*");
+    res.header(
+      "access-control-allow-headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if (req.method === "OPTIONS") {
+      res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET, OPTIONS");
+      return res.status(200).json();
+    }
+    next();
+  }
+);
 
 app.use('/workouts', workoutRoutes);
 
@@ -9,6 +29,21 @@ app.use((req, res, next) => {
     res.status(200).json({
         message: "Yay"
     });
-})
+});
+
+app.use((req, res, next) => {
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+});
 
 module.exports = app;
