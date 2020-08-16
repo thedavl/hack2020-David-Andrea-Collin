925cdbc9a9a4913e2457d02cc5a03febac395e78
Collin Dang <cdang38@cs.washington.edu>
2020-08-15 18:50:02 -0700

    added text fields

--- frontend/hack20/src/views/Create.vue
+++ frontend/hack20/src/views/Create.vue
@@ -1,7 +1,17 @@
 <template>
     <div>
-        <p>THIS IS THE CREATE PAGE</p>
+        <p class="createTitle">THIS IS THE CREATE PAGE</p>
+        <input name="workout_name" placeholder="workout name...">
+        <p> Type (select all that apply)</p>
+        <p> Total Time</p>
+        <p> Hours </p>
+        <input name="hours" >
+        <p> Minutes </p>
+        <input name="minutes">
     </div>
+        
+    
+
 </template>
 
 <script>
@@ -11,5 +21,6 @@ export default {
 </script>
 
 <style scoped>
-
+    p{  font-size: 14pt;
+        color:blue; }
 </style>
